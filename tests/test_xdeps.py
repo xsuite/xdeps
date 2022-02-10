6913240a14f3a7b6ad3b1ba8bab2d766cf4cf82d
Riccardo De Maria <riccardodemaria@gmail.com>
2022-02-10 13:49:11 +0100

    add test

--- tests/test_xdeps.py
+++ tests/test_xdeps.py
@@ -10,5 +10,21 @@ def test_set():
     assert r['c']==0.1*r['a']+0.2*r['b']
 
 
+def test_attrref():
+    class E:
+        pass
+
+    e=E()
+    e.knl=[0,1,2,3]
+    v={4:e,2:1.2}
+
+    mgr=xdeps.Manager()
+    v_=mgr.ref(v,'v')
+    v_[4].knl[1]=v_[2]*3
+
+    assert v[4].knl[1]==v[2]*3
+    v_[2]=1.1
+    assert v[4].knl[1]==v[2]*3
+
 
 
