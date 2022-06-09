f1a42ddbd6fc17dc9891b1bceba05f17b36cf323
giadarol <giovanni.iadarola@cern.ch>
2022-06-09 18:47:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
         name='xdeps',
         version='0.0.6',
         description='Data dependency manager',
-        long_description="Data dependency manager",
+        long_description=("Data dependency manager\n"
+                         "\nThis package is part of the Xsuite collection."),
         author='Riccardo De Maria',
         author_email='riccardo.de.maria@cern.ch',
         packages=find_packages(),
