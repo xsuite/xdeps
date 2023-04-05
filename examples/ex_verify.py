cd9932292348b4ea52ce8523ccbf325dd4f9a1a5
Riccardo De Maria <riccardodemaria@gmail.com>
2023-04-05 12:15:48 +0200

    update

---
+++ examples/ex_verify.py
@@ -0,0 +1,13 @@
+import xtrack as xt
+line = xt.Line.from_json(
+    './xtrack/test_data/hllhc15_noerrors_nobb/line_w_knobs_and_particle.json')
+
+manager = line._var_management['manager']
+
+for tt in list(manager.tasks):
+    print(tt)
+    manager.unregister(tt )
+    manager.verify(dcts=("rtasks", "deptasks", "tartasks"))
+
+manager.unregister(line.vars['cosi0'])
+manager.verify()
