e31ae14bb687bafab48d68e880cf40cde8719b80
giadarol <giovanni.iadarola@cern.ch>
2024-10-23 19:02:00 +0200

    Support capitals in expressions

--- xdeps/madxutils.py
+++ xdeps/madxutils.py
@@ -36,7 +36,7 @@ calc_grammar = """
          | NAME "(" sum ("," sum)* ")" -> call
          | "(" sum ")"
 
-    NAME: /[a-z_\\.][a-z0-9_\\.%]*/
+    NAME: /[A-z_\\.][A-z0-9_\\.%]*/
     %import common.NUMBER
     %import common.WS_INLINE
     %ignore WS_INLINE
