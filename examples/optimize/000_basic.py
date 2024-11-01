eae3122d7b9db83760b653ae3c58bcd27fb50af7
giadarol <giovanni.iadarola@cern.ch>
2024-11-01 16:57:51 +0100

    An assert

--- examples/optimize/000_basic.py
+++ examples/optimize/000_basic.py
@@ -1,4 +1,4 @@
-import numpy as np
+import xobjects as xo
 import xdeps as xd
 
 
@@ -10,4 +10,6 @@ x0 = [0., 0., 0.]
 opt = xd.Optimize.from_callable(my_function, x0=x0, steps=[1e-6, 1e-6, 1e-6, 1e-6],
                         tar=[0., 0., 0., 3.], tols=[1e-12, 1e-12, 1e-12, 1e-12])
 opt.solve()
+
+xo.assert_allclose(opt.get_merit_function().get_x(), [0.0001, 0.0003, -0.0005], atol=1e-6, rtol=0)
 
